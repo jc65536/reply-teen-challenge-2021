29d874d4fff75ffc0ef11d502da6895e66f7440b
Jason Cheng <jasoncheng224@gmail.com>
2021-03-10 20:56:04 -0800

    .

---
+++ train3/scoreboard/scoreboard.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<long long> vll;
+typedef vector<int> vi;
+
+ifstream in("scoreboard-in.txt");
+ofstream out("scoreboard-out.txt");
+
+struct team {
+    
+};
+
+void solve() {
+
+}
+
+int main() {
+    int T;
+    in >> T;
+    for (int i = 0; i < T; i++) {
+        out << "Case #" << i + 1 << ": ";
+        solve();
+        out << endl;
+    }
+}
+
